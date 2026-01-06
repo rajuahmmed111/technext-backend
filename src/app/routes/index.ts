649352c0343b409b8920b84007a3aa8db63c52cb
Raju Ahmmed <ahraju2003@gmail.com>
2026-01-07 02:15:42 +0600

    initiate project

---
+++ src/app/routes/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+import { authRoutes } from "../modules/Auth/auth.routes";
+import { userRoute } from "../modules/User/user.route";
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: "/users",
+    route: userRoute,
+  },
+  {
+    path: "/auth",
+    route: authRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
